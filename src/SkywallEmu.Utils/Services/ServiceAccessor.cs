6b4ce5edbff6c7bcfec722987c6f2e14d6d74eb7
Ovahlord <18347559+Ovahlord@users.noreply.github.com>
2026-02-25 21:57:23 +0100

    Utils: added ServiceAccessor class to access service instances from anywhere within the application

---
+++ src/SkywallEmu.Utils/Services/ServiceAccessor.cs
@@ -0,0 +1,37 @@
+// This file is part of the SkywallEmu project and is published under the
+// GPL 3.0. See LICENSE for further information.
+
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SkywallEmu.Utils.Services;
+
+public static class ServiceAccessor
+{
+    private static IServiceProvider? s_serviceProvider = null;
+
+    /// <summary>
+    /// Sets the service provider which contains the services of the application.
+    /// </summary>
+    /// <param name="serviceProvider">The instance of the ServiceProvider which will be used to access services</param>
+    public static void SetServiceProvider(IServiceProvider serviceProvider)
+    {
+        s_serviceProvider = serviceProvider;
+    }
+
+    /// <summary>
+    /// Attempts to retrieve a service instance of the given type
+    /// </summary>
+    /// <param name="service">The instance of the service, null when no service has been found</param>
+    /// <typeparam name="T">The type of the service to be retrieved</typeparam>
+    /// <returns>True, when a service of the given type has been found</returns>
+    public static bool TryGetService<T>([NotNullWhen(true)] out T? service) where T : class
+    {
+        service = null;
+        if (s_serviceProvider == null)
+            return false;
+
+        service = s_serviceProvider.GetService<T>();
+        return service != null;
+    }
+}
--- src/SkywallEmu/Program.cs
+++ src/SkywallEmu/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using SkywallEmu.Utils.Configuration;
+using SkywallEmu.Utils.Services;
 
 namespace SkywallEmu;
 
@@ -10,12 +11,14 @@ class Program
     {
         var builder = Host.CreateEmptyApplicationBuilder(null);
 
-        builder.Configuration.AddJsonFile("appsettings.json");
+        builder.Configuration.AddJsonFile("appsettings.json", false, false);
 
         // Register the loaded configuration in the AppSettings accessor class
         AppSettings.SetConfigurationManager(builder.Configuration);
 
         var app = builder.Build();
+
+        ServiceAccessor.SetServiceProvider(app.Services);
 
         await app.RunAsync();
     }
